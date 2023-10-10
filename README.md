4f1d0e4ee114c368e6c0cc405737bd470e34d58f
demond6374 <demond.roberson@gmail.com>
2023-10-09 23:37:35 -0500

    Configuring test script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + TypeScript + Vite
+# Redeeming Love University Built Using React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,17 +1,19 @@
-import { Box, Container, Image } from '@chakra-ui/react';
-import LogIn from './LogIn';
-import logo from '/assets/img/rlom_univ_logo.png';
+import { Box, Container, Image } from "@chakra-ui/react";
+import LogIn from "./LogIn";
 
 function SiteHeader() {
-
-    return (
-        <Container maxW='container.xl' pt={2} pb={2}>
-            <Box w='400px' h='160px'>
-                <Image src={logo} alt='RLOM University Logo' pb={2} /> 
-                <LogIn />   
-            </Box>
-        </Container>
-    );
+  return (
+    <Container maxW="container.xl" pt={2} pb={2}>
+      <Box w="400px" h="160px">
+        <Image
+          src="../assets/img/rlom_univ_logo.png"
+          alt="RLOM University Logo"
+          pb={2}
+        />
+        <LogIn />
+      </Box>
+    </Container>
+  );
 }
 
 export default SiteHeader;

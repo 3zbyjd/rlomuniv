fab01bcdf7a05a1eccd8672268ac5c5b8b94ce00
demond6374 <demond.roberson@gmail.com>
2024-01-24 16:19:16 +0000

    Changed theRLOM logo box width.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -4,7 +4,7 @@ import LogIn from "./LogIn";
 function SiteHeader() {
   return (
     <Container maxW="container.xl" pt={2} pb={2} bg='green.400'>
-      <Box w="400px" h="160px" bg="pink.100">
+      <Box w="75%" h="160px" bg="pink.100">
         <Image
           src="/assets/img/rlom_univ_logo.png"
           alt="RLOM University Logo"

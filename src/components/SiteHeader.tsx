450a9a55cd7b8b7c4876b24444171a8587f637b4
demond6374 <demond.roberson@gmail.com>
2024-01-24 18:34:56 +0000

    Adding a top grid to the page

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,10 +1,31 @@
-import { Box, Container, Image } from "@chakra-ui/react";
+import { Box, Container, Grid, GridItem, Image } from "@chakra-ui/react";
 import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
     <>
-    <Container maxW="50%" pt={2} pb={2} bg='green.400'>
+    <Grid
+      templateAreas={`"header"
+                      "main"
+                      "footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'150px 1fr'}
+      h='200px'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        Header
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
+    {/* <Container maxW="50%" pt={2} pb={2} bg='green.400'>
       <Box w="400px" h="160px" bg="pink.100">
         <Image
           src="/assets/img/rlom_univ_logo.png"
@@ -16,7 +37,7 @@ function SiteHeader() {
     </Container>
     <Container maxW="25%">
       <Box w="400px" bg="purple.100"><LogIn /></Box>
-    </Container>
+    </Container> */}
     </>
   );
 }

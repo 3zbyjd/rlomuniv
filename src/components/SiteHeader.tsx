4f1116f2a8b225e703da4a88bc4e318892c60d28
demond6374 <demond.roberson@gmail.com>
2024-01-24 20:48:37 +0000

    Messing with chakra grid.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,31 +1,33 @@
-import { Grid, GridItem} from "@chakra-ui/react";
+import {Container, Grid, GridItem} from "@chakra-ui/react";
 // import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
     <>
-    <Grid
-      templateAreas={`"header"
-                      "main main"
-                      "footer"`}
-      gridTemplateRows={'50px 1fr 30px'}
-      gridTemplateColumns={'150px 1fr'}
-      h='200px'
-      maxW="100%"
-      gap='1'
-      color='blackAlpha.700'
-      fontWeight='bold'
-    >
-      <GridItem pl='2' bg='orange.300' area={'header'}>
-        Header
-      </GridItem>
-      <GridItem pl='2' bg='green.300' area={'main'}>
-        Main
-      </GridItem>
-      <GridItem pl='2' bg='blue.300' area={'footer'}>
-        Footer
-      </GridItem>
-    </Grid>
+    <Container maxW="50%" pt={2} pb={2} bg='green.400'>
+      <Grid
+        templateAreas={`"header"
+                        "main main"
+                        "footer"`}
+        gridTemplateRows={'50px 1fr 30px'}
+        gridTemplateColumns={'150px 1fr'}
+        h='200px'
+        maxW="100%"
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <GridItem pl='2' bg='orange.300' area={'header'}>
+          Header
+        </GridItem>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+        <GridItem pl='2' bg='blue.300' area={'footer'}>
+          Footer
+        </GridItem>
+      </Grid>
+    </Container>
     {/* <Container maxW="50%" pt={2} pb={2} bg='green.400'>
       <Box w="400px" h="160px" bg="pink.100">
         <Image

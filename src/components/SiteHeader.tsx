22e6b1fcbe660abfaf96dd82b9a16c3db1ed3a8d
demond6374 <demond.roberson@gmail.com>
2024-01-24 15:56:10 +0000

    Changed the background color of the components to see layout.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -3,15 +3,15 @@ import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
-    <Container maxW="container.xl" pt={2} pb={2}>
-      <Box w="400px" h="160px">
+    <Container maxW="container.xl" pt={2} pb={2} bg='green.400'>
+      <Box w="400px" h="160px" bg="pink.100">
         <Image
           src="/assets/img/rlom_univ_logo.png"
           alt="RLOM University Logo"
           pb={2}
         />       
       </Box>
-      <Box><LogIn /></Box>
+      <Box bg="purple.100"><LogIn /></Box>
     </Container>
   );
 }

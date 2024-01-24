03f4432bab3d305f8059558fff79d4e82c1a9ba7
demond6374 <demond.roberson@gmail.com>
2024-01-24 21:53:24 +0000

    Messing with chakra UI elements.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -4,7 +4,7 @@ import {Container, Grid, GridItem} from "@chakra-ui/react";
 function SiteHeader() {
   return (
     <>
-    <Container maxW="50%" pt={2} pb={2} bg='green.400'>
+    <Container maxW="100%" pt={2} pb={2}>
       <Grid
         templateAreas={`"header"
                         "mainL mainR"`}

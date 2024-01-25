e317f1a64279ca1d5862bf268d1a105994558e7e
demond6374 <demond.roberson@gmail.com>
2024-01-25 05:35:29 +0000

    Messing with chakra ui table.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -21,6 +21,7 @@ function SiteHeader() {
           <Image
               src="/assets/img/rlom_univ_logo.png"
               alt="RLOM University Logo"
+              pt={2}
               pb={2}
               w="400px"
               h="160px"

5463c299828fb7313bab6c1c8c8a8ccad1d3a029
demond6374 <demond.roberson@gmail.com>
2024-01-25 16:15:25 +0000

    Messing with Chakra UI Grid.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -4,7 +4,7 @@ import LogIn from "./LogIn";
 function SiteHeader() {
   return (
     <>
-    <Container maxW="container.lg">
+    <Container maxW="container.xl">
       <Grid
         templateAreas={`"mainL header"
                         "mainL mainR"`}

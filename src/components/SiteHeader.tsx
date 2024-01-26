1774fb17544875007b6199a45076f896645d9858
demond6374 <demond.roberson@gmail.com>
2024-01-26 04:51:51 +0000

    Made changes to elements.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,9 +1,33 @@
-import {Box, Container, Image} from "@chakra-ui/react";
+import {Box, Container, Grid, GridItem, Image} from "@chakra-ui/react";
 import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
     <>
+    <Grid
+      templateAreas={`"header header"
+                      "nav main"
+                      "nav footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'150px 1fr'}
+      h='200px'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        Header
+      </GridItem>
+      <GridItem pl='2' bg='pink.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid>
     <Container maxW="50%" pt={2} pb={2} bg='green.400'>
       <Box w="400px" h="160px" bg="pink.100">
         <Image

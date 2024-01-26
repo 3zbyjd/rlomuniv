8d83c52e14c1aea3dce13d9c45fe0faabf1e7828
demond6374 <demond.roberson@gmail.com>
2024-01-26 05:05:44 +0000

    Made changes to elements.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,30 +1,32 @@
-import {Grid, GridItem} from "@chakra-ui/react";
+import {Container, Grid, GridItem} from "@chakra-ui/react";
 // import {Box, Container, Grid, GridItem, Image} from "@chakra-ui/react";
 // import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
     <>
-    <Grid
-      templateAreas={`"Logo Header"
-                      "Logo HeaderBody"`}
-      gridTemplateRows={'50px 1fr'}
-      gridTemplateColumns={'400px 1fr'}
-      h='200px'
-      gap='1'
-      color='blackAlpha.700'
-      fontWeight='bold'
-    >
-      <GridItem pl='2' bg='orange.300' area={'Logo'}>
-        Logo
-      </GridItem>
-      <GridItem pl='2' bg='pink.300' area={'Header'}>
-        Header
-      </GridItem>
-      <GridItem pl='2' bg='green.300' area={'HeaderBody'}>
-        Header Body
-      </GridItem>
-    </Grid>
+    <Container maxW='container.xl'>
+      <Grid
+        templateAreas={`"Logo Header"
+                        "Logo HeaderBody"`}
+        gridTemplateRows={'50px 1fr'}
+        gridTemplateColumns={'400px 1fr'}
+        h='200px'
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <GridItem pl='2' bg='orange.300' area={'Logo'}>
+          Logo
+        </GridItem>
+        <GridItem pl='2' bg='pink.300' area={'Header'}>
+          Header
+        </GridItem>
+        <GridItem pl='2' bg='green.300' area={'HeaderBody'}>
+          Header Body
+        </GridItem>
+      </Grid>
+    </Container>
     {/* <Container maxW="50%" pt={2} pb={2} bg='green.400'>
       <Box w="400px" h="160px" bg="pink.100">
         <Image

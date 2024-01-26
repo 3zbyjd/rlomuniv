597a9bddf89c8702c6b7cebad83967946dc5e76e
demond6374 <demond.roberson@gmail.com>
2024-01-26 15:03:31 +0000

    Adding the Login buttons back to the header.

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,6 +1,5 @@
 import {Center, Container, Grid, GridItem, Image} from "@chakra-ui/react";
-// import {Box, Container, Grid, GridItem, Image} from "@chakra-ui/react";
-// import LogIn from "./LogIn";
+import LogIn from "./LogIn";
 
 function SiteHeader() {
   return (
@@ -25,7 +24,7 @@ function SiteHeader() {
           </Center>
         </GridItem>
         <GridItem pl='2' bg='pink.300' area={'Header'}>
-          Header
+          <LogIn />
         </GridItem>
         <GridItem pl='2' bg='green.300' area={'HeaderBody'}>
           Header Body

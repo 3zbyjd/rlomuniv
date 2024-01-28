d5c008cfea68a8748b68514f50422cc92d7dff5b
demond6374 <demond.roberson@gmail.com>
2024-01-28 05:51:29 +0000

    Added an interface so that we can start to implement lists.

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -13,193 +13,60 @@ import {
   Container,
 } from "@chakra-ui/react";
 
-function CourseCard() {
-  return (
+interface Course {
+  id: number;
+  imageName: string;
+  imageSrc: string;
+  courseHeading: string;
+  courseDescription: string;
+  formattedCoursePrice: string;
+}
+
+interface Props {
+  courses: Course[];
+}
+
+const CourseList = ({ courses }: Props) => {
+  return(
     <>
       <Container maxWidth="container.xl">
         <SimpleGrid
           spacing={4}
           templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
         >
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/code_screen.jpg"
-                alt="python course logo"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Learn to code python</Heading>
-                <Text>Learn to code in python by building a web scraper.</Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $25
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/code_screen2.jpg"
-                alt="react code logo"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Learn react </Heading>
-                <Text>
-                  Learn to build responsive web applications with react.
-                </Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $20
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/fitness1.jpg"
-                alt="fitness1 Logo"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Get in shape and stay in shape</Heading>
-                <Text>
-                  Learn to use nothing but bodyweight to get in shape. Learn
-                  receipies that will fuel fat burnig
-                </Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $40
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/cross_sm.jpg"
-                alt="Welcome Logo"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Welcome to the family</Heading>
-                <Text>So, you&apos;ve excepted Christ. Now what?</Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $50
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/group_coding.jpg"
-                alt="git course logo"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">
-                  Learn to use git for collaborative coding
-                </Heading>
-                <Text>
-                  Git is a popular code repo. Learn to use it for collaborative
-                  coding.
-                </Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $30
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-          <Card maxW="xs">
-            <CardBody>
-              <Image
-                src="/assets/img/course_logos/business_table.jpg"
-                alt="Learn to plan and start your own business"
-                borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Living room Sofa</Heading>
-                <Text>
-                  Starting a business can be a very daunting task. This course
-                  will give you a step by step plan to start your own business.
-                </Text>
-                <Text color="blue.600" fontSize="2xl">
-                  $35
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
+          {courses.map(course => 
+            <Card maxW="xs" key={course.id}>
+              <CardBody>
+                <Image
+                  src={course.imageSrc}
+                  alt={course.imageName}
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">{course.courseHeading}</Heading>
+                  <Text>{course.courseDescription}</Text>
+                  <Text color="blue.600" fontSize="2xl">
+                    {course.formattedCoursePrice}
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Buy now
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    Add to cart
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+            )}
         </SimpleGrid>
       </Container>
     </>
-  );
+  )
 }
 
-export default CourseCard;
+export default CourseList;

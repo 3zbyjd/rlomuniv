bdbc1f71aa3db0c4638aa7fb42efd3c68ba26799
demond6374 <demond.roberson@gmail.com>
2024-01-29 03:15:52 +0000

    Removed un-needed comments

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -30,20 +30,7 @@ function SiteHeader() {
           Header Body
         </GridItem>
       </Grid>
-    </Container>
-    {/* <Container maxW="50%" pt={2} pb={2} bg='green.400'>
-      <Box w="400px" h="160px" bg="pink.100">
-        <Image
-          src="/assets/img/rlom_univ_logo.png"
-          alt="RLOM University Logo"
-          pb={2}
-        />       
-      </Box>
-      
-    </Container>
-    <Container maxW="25%">
-      <Box w="400px" bg="purple.100"><LogIn /></Box>
-    </Container> */}
+    </Container>    
     </>
   );
 }

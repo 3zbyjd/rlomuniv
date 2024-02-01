83e52dce6c5fa73726e7307fcb809a0624b5be4a
demond6374 <demond.roberson@gmail.com>
2024-02-01 21:59:53 +0000

    Experimenting with positioning a button to the right or left.

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -19,7 +19,7 @@ function LogIn() {
         </text>
       </Box>
       <Box bg='tomato'>
-        <Button colorScheme="purple" variant="solid" size='xs' position='fixed' right='30px'>
+        <Button colorScheme="purple" variant="solid" size='xs' position='relative' left='30px'>
           Log Out&nbsp;
           <BiSolidLogOut />
         </Button>

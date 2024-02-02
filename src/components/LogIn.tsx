39cb49099956197900685599ee1d5409c3f56cd6
demond6374 <demond.roberson@gmail.com>
2024-02-02 03:49:02 +0000

    Editing styles

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -11,19 +11,18 @@ function LogIn() {
     return (
       <>
       <Grid templateColumns='repeat(4, 1fr)' gap={1}>
-      <GridItem bg='tomato'></GridItem>
-      {/* <GridItem bg='tomato'></GridItem> */}
-      <GridItem bg='tomato' colSpan={2}>
-        <text>
-          Logged in as&#58; Demond Roberson
-        </text>
-      </GridItem>
-      <GridItem bg='tomato'>
-        <Button colorScheme="purple" variant="solid" size='xs' position='relative' left='130px'>
-          Log Out&nbsp;
-          <BiSolidLogOut />
-        </Button>
-      </GridItem>
+        <GridItem></GridItem>
+        <GridItem colSpan={2}>
+          <text>
+            Logged in as&#58; Demond Roberson
+          </text>
+        </GridItem>
+        <GridItem bg='tomato'>
+          <Button colorScheme="purple" variant="solid" size='xs' position='relative' left='130px'>
+            Log Out&nbsp;
+            <BiSolidLogOut />
+          </Button>
+        </GridItem>
       </Grid>        
       </>      
     );    
@@ -32,20 +31,20 @@ function LogIn() {
   return (
     <>
     <Grid templateColumns='repeat(4, 1fr)' gap={1}>
-    <GridItem></GridItem>
-    <GridItem></GridItem>
-    <GridItem>
-      <Button colorScheme='purple' variant='solid' size='xs'>
-        Log In&nbsp;
-        <BiSolidLogIn />
-      </Button>
-    </GridItem>
-    <GridItem>
-      <Button colorScheme='purple' variant='solid' size='xs' position='relative' left='130px'>
-        Sign Up&nbsp;
-        <IoMdCreate />
-      </Button>
-    </GridItem>
+      <GridItem></GridItem>
+      <GridItem></GridItem>
+      <GridItem>
+        <Button colorScheme='purple' variant='solid' size='xs'>
+          Log In&nbsp;
+          <BiSolidLogIn />
+        </Button>
+      </GridItem>
+      <GridItem>
+        <Button colorScheme='purple' variant='solid' size='xs' position='relative' left='130px'>
+          Sign Up&nbsp;
+          <IoMdCreate />
+        </Button>
+      </GridItem>
     </Grid>
     </>
   );

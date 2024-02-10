8311277fad65c09900ae1baa7afb7d2a2b7c79f1
demond6374 <demond.roberson@gmail.com>
2024-02-10 12:54:38 -0600

    Working to implement pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import CourseCardList from "./components/CourseCard";
 import SiteHeader from "./components/SiteHeader";
-import Pagination from "./components/pagination";
 
 function App() {
   return (
     <>
       <SiteHeader />
       <CourseCardList />
-      <Pagination />
     </>
   );
 }

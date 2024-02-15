8032fc8abd16ff2013b1130a52ce26afebbec5e2
demond6374 <demond.roberson@gmail.com>
2024-02-14 21:37:42 -0600

    Implementing routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+//import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+
+const router = createBrowserRouter([]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );

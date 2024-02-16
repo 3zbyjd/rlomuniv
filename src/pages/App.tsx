dee2584f33c560f3eef322588322ec05523dc336
demond6374 <demond.roberson@gmail.com>
2024-02-15 22:20:22 -0600

    Adding routes

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import CourseCardList from "./components/CourseCard";
+import SiteHeader from "./components/SiteHeader";
+
+function App() {
+  return (
+    <>
+      <SiteHeader />
+      <CourseCardList />
+    </>
+  );
+}
+
+export default App;

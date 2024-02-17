8e1420a7cc0e58bd6992a2b788901bcf38cc0e15
demond6374 <demond.roberson@gmail.com>
2024-02-16 22:25:03 -0600

    Adding page routes to main page.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,36 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./pages/App/App";
+import Course from "./pages/Course/Course";
+import CourseDetails from "./pages/CourseDetails/CourseDetails";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Profile from "./pages/Profile/Profile";
+import User from "./pages/User/User";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/Course",
+    element: <Course />,
+  },
+  {
+    path: "/CourseDetails",
+    element: <CourseDetails />,
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/Profile",
+    element: <Profile />,
+  },
+  {
+    path: "/User",
+    element: <User />,
   },
 ]);
 
---
+++ src/pages/CourseDetails/CourseDetails.tsx
@@ -0,0 +1,9 @@
+function CourseDetails() {
+  return (
+    <div>
+      <h1>Course Details</h1>
+    </div>
+  );
+}
+
+export default CourseDetails;

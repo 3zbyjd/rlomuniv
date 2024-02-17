16cd4e9ebab0fd053933aa18107327a8725a898c
demond6374 <demond.roberson@gmail.com>
2024-02-16 22:08:20 -0600

    Moved pages into page folders.

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from "./pages/App";
+import App from "./pages/App/App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

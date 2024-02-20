f29b1087870052120e3c9c37d5fc9c9cf935fd49
demond6374 <demond.roberson@gmail.com>
2024-02-19 18:26:27 -0600

    Building edit user form.

--- src/pages/User/EditUser.tsx
+++ src/pages/User/EditUser.tsx
@@ -1,34 +1,56 @@
-import { FormControl, FormLabel } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { MdOutlineAttachEmail } from "react-icons/md";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 import UserType from "../../components/UserType";
 
 function EditUser() {
-    return (
-        <>
-            <FormControl>
-                <FormLabel>
-                    First Name
-                    <input type="text" placeholder="Enter Firstname" />
-                </FormLabel>
-                
-                <FormLabel>
-                    Last Name
-                    <input type="text" placeholder="Enter Firstname" />
-                </FormLabel>
-                
-                <FormLabel>
-                    Email
-                    <input type="email" placeholder="Enter Email" />
-                </FormLabel>
-                
-                <FormLabel>
-                    Phone
-                    <input type="tel" placeholder="Enter Phone Number" />
-                </FormLabel>
-                
-                <UserType />
-            </FormControl>
-        </>
-    )
+  return (
+    <>
+      <FormControl>
+        <FormLabel>
+          First Name
+          <br />
+          <Input type="text" placeholder="Enter Firstname" />
+        </FormLabel>
+
+        <FormLabel>
+          Last Name
+          <br />
+          <Input type="text" placeholder="Enter Firstname" />
+        </FormLabel>
+
+        <FormLabel>
+          Email
+          <br />
+          <InputGroup>
+            <InputLeftAddon>
+              <MdOutlineAttachEmail />
+            </InputLeftAddon>
+            <Input type="email" placeholder="Enter Email" />
+          </InputGroup>
+        </FormLabel>
+
+        <FormLabel>
+          Phone
+          <br />
+          <InputGroup>
+            <InputLeftAddon>
+              <FaPhoneSquareAlt />
+            </InputLeftAddon>
+            <Input type="tel" placeholder="Enter Phone Number" />
+          </InputGroup>
+        </FormLabel>
+
+        <UserType />
+      </FormControl>
+    </>
+  );
 }
 
 export default EditUser;

d704fe695faae7ac0c5c69dec0a652ecec811475
demond6374 <demond.roberson@gmail.com>
2024-02-20 18:21:50 -0600

    Finishing up the user edit form.

--- src/pages/User/EditUser.tsx
+++ src/pages/User/EditUser.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Divider,
   FormControl,
   FormLabel,
@@ -18,7 +19,7 @@ function EditUser() {
       <SiteHeader />
       <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
       <Box
-        bg="#ddccff"
+        bg="#cccccc"
         maxW="40%"
         padding="4"
         marginTop="25px"
@@ -30,13 +31,21 @@ function EditUser() {
           <FormLabel>
             First Name
             <br />
-            <Input type="text" placeholder="Enter Firstname" />
+            <Input
+              type="text"
+              placeholder="Enter Firstname"
+              focusBorderColor="purple.400"
+            />
           </FormLabel>
 
           <FormLabel>
             Last Name
             <br />
-            <Input type="text" placeholder="Enter Firstname" />
+            <Input
+              type="text"
+              placeholder="Enter Firstname"
+              focusBorderColor="purple.400"
+            />
           </FormLabel>
 
           <FormLabel>
@@ -46,7 +55,11 @@ function EditUser() {
               <InputLeftAddon>
                 <MdOutlineAttachEmail />
               </InputLeftAddon>
-              <Input type="email" placeholder="Enter Email" />
+              <Input
+                type="email"
+                placeholder="Enter Email"
+                focusBorderColor="purple.400"
+              />
             </InputGroup>
           </FormLabel>
 
@@ -57,11 +70,49 @@ function EditUser() {
               <InputLeftAddon>
                 <FaPhoneSquareAlt />
               </InputLeftAddon>
-              <Input type="tel" placeholder="Enter Phone Number" />
+              <Input
+                type="tel"
+                placeholder="Enter Phone Number"
+                focusBorderColor="purple.400"
+              />
+            </InputGroup>
+          </FormLabel>
+
+          <FormLabel>
+            Mailing Address
+            <br />
+            <InputGroup>
+              <InputLeftAddon>
+                <FaPhoneSquareAlt />
+              </InputLeftAddon>
+              <Input
+                type="text"
+                placeholder="Enter Mailing Address"
+                focusBorderColor="purple.400"
+              />
+            </InputGroup>
+          </FormLabel>
+
+          <FormLabel>
+            Apt/Suite Number
+            <br />
+            <InputGroup>
+              <InputLeftAddon>
+                <FaPhoneSquareAlt />
+              </InputLeftAddon>
+              <Input
+                type="text"
+                placeholder="Enter Apt/Suite Number"
+                focusBorderColor="purple.400"
+              />
             </InputGroup>
           </FormLabel>
 
           <UserType />
+
+          <Button colorScheme="purple.400" size="md">
+            Button
+          </Button>
         </FormControl>
       </Box>
     </>

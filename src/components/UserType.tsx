31116256bca1bc241692e1ba17eed932dbb0f426
demond6374 <demond.roberson@gmail.com>
2024-02-20 19:04:19 -0600

    Finishing up the user edit form.

--- src/components/UserType.tsx
+++ src/components/UserType.tsx
@@ -4,23 +4,29 @@
 //  Administrator
 //  SystemAdministrator
 
-import { Select } from "@chakra-ui/react";
+import { FormLabel, Select } from "@chakra-ui/react";
 import userTypesData from "../../public/assets/usertype.json";
 import { useState } from "react";
 
-function UserType(){
-const [userType, setUserType] = useState(userTypesData);
-setUserType;
+function UserType() {
+  const [userType, setUserType] = useState(userTypesData);
+  setUserType;
 
-    return(
-        <>
-            <Select placeholder='Select user type'>
-                {userType.map((ut) => (
-                    <option key={ut.id} value={ut.userType}>{ut.userType}</option>
-                ))}
-            </Select>
-        </>
-    );
+  return (
+    <>
+      <FormLabel>
+        User Type
+        <br />
+        <Select placeholder="Select user type">
+          {userType.map((ut) => (
+            <option key={ut.id} value={ut.userType}>
+              {ut.userType}
+            </option>
+          ))}
+        </Select>
+      </FormLabel>
+    </>
+  );
 }
 
 export default UserType;

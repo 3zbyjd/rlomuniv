c41b850460028614e7fa1361536e0afee774f025
demond6374 <demond.roberson@gmail.com>
2024-02-20 22:10:16 -0600

    Finishing up the user edit form.

--- src/pages/User/EditUser.tsx
+++ src/pages/User/EditUser.tsx
@@ -19,7 +19,7 @@ function EditUser() {
       <SiteHeader />
       <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
       <Box
-        bg="grey.400"
+        bg="grey.100"
         maxW="40%"
         padding="4"
         marginTop="25px"

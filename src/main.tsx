1475eb7d49dd2f815d11065c4ca4be1cf9104ea7
demond6374 <demond.roberson@gmail.com>
2024-02-21 21:05:57 -0600

    Finishing up the user edit form.

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import CourseDetails from "./pages/CourseDetails/CourseDetails";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Profile from "./pages/Profile/Profile";
 import EditUser from "./pages/User/EditUser";
-// import UserList from "./pages/User/UserList";
+import UserList from "./pages/User/UserList";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +34,10 @@ const router = createBrowserRouter([
   {
     path: "/EditUser",
     element: <EditUser />,
+  },
+  {
+    path: "/UserList",
+    element: <UserList />,
   },
 ]);
 

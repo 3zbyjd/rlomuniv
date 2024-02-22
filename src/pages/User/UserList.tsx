863dfa36bb302ffde0ab7f7cfbb0afcf83bb9b6b
demond6374 <demond.roberson@gmail.com>
2024-02-21 22:42:02 -0600

    Finishing up the user edit form.

--- src/pages/User/UserList.tsx
+++ src/pages/User/UserList.tsx
@@ -1,24 +1,23 @@
-// import { Grid, GridItem } from "@chakra-ui/react";
-// import UserListData from "../../../public/assets/users.json";
-// import { useState } from "react";
-import { Divider } from "@chakra-ui/react";
+import { Divider, Grid, GridItem } from "@chakra-ui/react";
+import UserListData from "../../../public/assets/users.json";
+import { useState } from "react";
 import SiteHeader from "../../components/SiteHeader";
 
-// interface UserProp {
-//   id: number;
-//   username: string;
-//   firstname: string;
-//   lastname: string;
-//   email: string;
-//   address1: string;
-//   address2: string;
-//   phone1: string;
-//   userType: string;
-//   password: string;
-// }
+interface UserProp {
+  id: number;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  address1: string;
+  address2: string;
+  phone1: string;
+  userType: string;
+  password: string;
+}
 
-// const [userList, setUserList] = useState<UserProp[]>(UserListData);
-// setUserList;
+const [userList, setUserList] = useState<UserProp[]>(UserListData);
+setUserList;
 
 function UserList() {
   return (
@@ -30,24 +29,24 @@ function UserList() {
         height="2"
         marginBottom="25px"
       />
-      {/* //       <Grid templateColumns="repeat(7, 1fr)" gap={1}>
-//         {userList.map((userData) => (
-//           <>
-//             <GridItem w="100%" bg="gray.100">
-//               {userData.firstname} {userData.lastname}
-//             </GridItem>
-//             <GridItem w="100%" bg="gray.100">
-//               {userData.email}
-//             </GridItem>
-//             <GridItem w="100%" bg="gray.100">
-//               {userData.address1} {userData.address2}
-//             </GridItem>
-//             <GridItem w="100%" bg="gray.100">
-//               {userData.phone1}
-//             </GridItem>
-//           </>
-//         ))}
-//       </Grid> */}
+      <Grid templateColumns="repeat(4, 1fr)" gap={1}>
+        {userList.map((userData) => (
+          <>
+            <GridItem w="100%" bg="gray.100">
+              {userData.firstname} {userData.lastname}
+            </GridItem>
+            <GridItem w="100%" bg="gray.100">
+              {userData.email}
+            </GridItem>
+            <GridItem w="100%" bg="gray.100">
+              {userData.address1} {userData.address2}
+            </GridItem>
+            <GridItem w="100%" bg="gray.100">
+              {userData.phone1}
+            </GridItem>
+          </>
+        ))}
+      </Grid>
     </>
   );
 }

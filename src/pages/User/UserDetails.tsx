9e15c5c862f046cb9e860c604165314bae3a0061
demond6374 <demond.roberson@gmail.com>
2024-02-21 22:26:53 -0600

    Finishing up the user edit form.

---
+++ src/pages/User/UserDetails.tsx
@@ -0,0 +1,9 @@
+function UserDetails() {
+  return (
+    <div>
+      <h1>UserDetails</h1>
+    </div>
+  );
+}
+
+export default UserDetails;
--- src/pages/User/UserList.tsx
+++ src/pages/User/UserList.tsx
@@ -1,48 +1,58 @@
-import { Grid, GridItem } from "@chakra-ui/react";
-import UserListData from "../../../public/assets/users.json";
-import { useState } from "react";
-import SiteHeader from "../../components/SiteHeader";
-
-interface UserProp {
-  id: number;
-  username: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-  address1: string;
-  address2: string;
-  phone1: string;
-  userType: string;
-  password: string;
-}
-
-const [userList, setUserList] = useState<UserProp[]>(UserListData);
-setUserList;
-
 function UserList() {
   return (
-    <>
-      <SiteHeader />
-      <Grid templateColumns="repeat(7, 1fr)" gap={1}>
-        {userList.map((userData) => (
-          <>
-            <GridItem w="100%" bg="gray.100">
-              {userData.firstname} {userData.lastname}
-            </GridItem>
-            <GridItem w="100%" bg="gray.100">
-              {userData.email}
-            </GridItem>
-            <GridItem w="100%" bg="gray.100">
-              {userData.address1} {userData.address2}
-            </GridItem>
-            <GridItem w="100%" bg="gray.100">
-              {userData.phone1}
-            </GridItem>
-          </>
-        ))}
-      </Grid>
-    </>
+    <div>
+      <h1>UserList</h1>
+    </div>
   );
 }
 
 export default UserList;
+
+// import { Grid, GridItem } from "@chakra-ui/react";
+// import UserListData from "../../../public/assets/users.json";
+// import { useState } from "react";
+// import SiteHeader from "../../components/SiteHeader";
+
+// interface UserProp {
+//   id: number;
+//   username: string;
+//   firstname: string;
+//   lastname: string;
+//   email: string;
+//   address1: string;
+//   address2: string;
+//   phone1: string;
+//   userType: string;
+//   password: string;
+// }
+
+// const [userList, setUserList] = useState<UserProp[]>(UserListData);
+// setUserList;
+
+// function UserList() {
+//   return (
+//     <>
+//       <SiteHeader />
+//       <Grid templateColumns="repeat(7, 1fr)" gap={1}>
+//         {userList.map((userData) => (
+//           <>
+//             <GridItem w="100%" bg="gray.100">
+//               {userData.firstname} {userData.lastname}
+//             </GridItem>
+//             <GridItem w="100%" bg="gray.100">
+//               {userData.email}
+//             </GridItem>
+//             <GridItem w="100%" bg="gray.100">
+//               {userData.address1} {userData.address2}
+//             </GridItem>
+//             <GridItem w="100%" bg="gray.100">
+//               {userData.phone1}
+//             </GridItem>
+//           </>
+//         ))}
+//       </Grid>
+//     </>
+//   );
+// }
+
+// export default UserList;

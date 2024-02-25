321a30becae737ea3f0fca0e5031437ee3971b27
demond6374 <demond.roberson@gmail.com>
2024-02-25 17:31:29 -0600

    Working on implementing the footer.

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,10 +1,22 @@
-import { Divider } from "@chakra-ui/react";
+import { Container, Divider, Grid, GridItem, Image } from "@chakra-ui/react";
 
 function SiteFooter() {
     return (
         <>
             <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
-            <div>Footer</div>
+            <Container backgroundImage="/assets/img/rlom_univ_logo.png">
+            <Grid
+                h='200px'
+                templateRows='repeat(2, 1fr)'
+                templateColumns='repeat(5, 1fr)'
+                gap={4}
+            >
+                <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+                <GridItem colSpan={2} bg='papayawhip' />
+                <GridItem colSpan={2} bg='papayawhip' />
+                <GridItem colSpan={4} bg='tomato' />
+            </Grid>
+            </Container>
         </>
     )
 }

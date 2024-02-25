098a46c0b3ee96364d8cccc51e449db584bad9f9
demond6374 <demond.roberson@gmail.com>
2024-02-25 17:57:41 -0600

    Working on implementing the footer.

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -4,9 +4,8 @@ function SiteFooter() {
     return (
         <>
             <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
-            {/* <Container width="100%" backgroundImage="/assets/img/blackbrickBG.jpg"> */}
             <Grid
-                h='200px'
+                h='300px'
                 templateRows='repeat(2, 1fr)'
                 templateColumns='repeat(5, 1fr)'
                 gap={4}

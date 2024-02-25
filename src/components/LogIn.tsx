d2c29ee33553d985e37b7d0cb8d3a6ca8cc67af3
demond6374 <demond.roberson@gmail.com>
2024-02-24 23:07:40 -0600

    Working to render user list route.

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -14,7 +14,7 @@ function LogIn() {
           direction={"row"}
           spacing={6}
           backgroundColor={"tomato"}
-          width="30%"
+          width="50%"
         >
           <Box>
             <text>Logged in as&#58; Demond Roberson</text>

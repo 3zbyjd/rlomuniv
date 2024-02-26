c4615dfbc707dd9802cdbc0732a335d3216ffaaf
demond6374 <demond.roberson@gmail.com>
2024-02-25 20:12:09 -0600

    Implementing footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,24 +1,24 @@
-import { Divider, Grid, GridItem} from "@chakra-ui/react";
+import { Divider, Grid, GridItem } from "@chakra-ui/react";
 
 function SiteFooter() {
-    return (
-        <>
-            <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
-            <Grid
-                h='250px'
-                templateRows='repeat(2, 1fr)'
-                templateColumns='repeat(5, 1fr)'
-                gap={4}
-                backgroundImage="/assets/img/blackbrickBG.jpg"
-            >
-                <GridItem rowSpan={2} colSpan={1} />
-                <GridItem colSpan={2} />
-                <GridItem colSpan={2} />
-                <GridItem colSpan={4} />
-            </Grid>
-            {/* </Container> */}
-        </>
-    )
+  return (
+    <>
+      <Divider orientation="horizontal" bg="#4d4c4c" height="2" />
+      <Grid
+        h="250px"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(5, 1fr)"
+        gap={4}
+        backgroundImage="/assets/img/BlackBrickBG2.png"
+      >
+        <GridItem rowSpan={2} colSpan={1} />
+        <GridItem colSpan={2} />
+        <GridItem colSpan={2} />
+        <GridItem colSpan={4} />
+      </Grid>
+      {/* </Container> */}
+    </>
+  );
 }
 
 export default SiteFooter;

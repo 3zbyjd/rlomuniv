6da786a47b47b90041317c1bab6a44420157fca9
demond6374 <demond.roberson@gmail.com>
2024-03-15 23:12:19 -0500

    Continued work with stripe api

--- src/components/SingleItemCheckout.tsx
+++ src/components/SingleItemCheckout.tsx
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
 import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
-import CheckoutPage from "./CheckoutPage";
+import CheckoutForm from "./CheckoutForm";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_rLzK97qwfIVJIK4W3AQnGdK1");
+const stripePromise = await loadStripe("pk_test_rLzK97qwfIVJIK4W3AQnGdK1");
 
 function SingleItemCheckout() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
   const options = {
     business: "RedeemingLove",
     mode: "payment",
@@ -24,11 +38,17 @@ function SingleItemCheckout() {
   };
 
   let newOptons = options as StripeElementsOptions;
-
+  console.log(newOptons);
+  console.log(clientSecret);
+  console.log(stripePromise);
   return (
+    // <div className="App">
+    //   {clientSecret && (
     <Elements stripe={stripePromise} options={newOptons}>
-      <CheckoutPage />
+      <CheckoutForm />
     </Elements>
+    //   )}
+    // </div>
   );
 }
 
